0f1156fca7f5c9a43257ecdf19aa4bf9d661d3cc
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-12 18:36:34 +1200

    Add and implement "clear grid" button

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const container = document.querySelector("#container");
+let gridSize = 16;
 
-let gridSize = 64;
+const container = document.querySelector("#container");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", clearGrid);
 
 createGrid(gridSize);
+
+function clearGrid() {
+    container.replaceChildren();
+    getGridSize();
+    createGrid(gridSize);
+}
+
+function getGridSize() {
+    do {
+        gridSize = +prompt("Enter a number from 16 to 100.");
+    } while ( 16 > gridSize || gridSize > 100);
+}
+
+
 
 function createGrid(size) {
     const pixelHeight = (container.offsetHeight / size);
@@ -12,7 +29,7 @@ function createGrid(size) {
         const pixel = document.createElement("div");
         pixel.style.height = `${pixelHeight}px`;
         pixel.style.width = `${pixelWidth}px`;
-        pixel.addEventListener("mouseover", colorPixel );
+        pixel.addEventListener("mouseover", colorPixel);
         container.appendChild(pixel);
     }
 };
@@ -21,5 +38,4 @@ function colorPixel(e) {
     e.stopPropagation();
     const pixel = e.target;
     pixel.style.backgroundColor = "green";
-    console.log(e.target);
 }
