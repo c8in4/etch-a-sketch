a35c9ae0bc004499b5f52444b79be599386c9bdd
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-06-19 12:20:22 +1200

    Change paintPixel() to paint in random color

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const container = document.querySelector('#container')
+const button = document.querySelector('#button')
 
-let standardGridSize = 16
-
-function createGrid(gridSize = standardGridSize) {
-    for (let i = 0; i < gridSize ** 2; i++) {
-        const pixel = document.createElement('div')
-        pixel.style.width = container.offsetWidth / gridSize + 'px'
-        pixel.classList.add('pixel')
-        container.appendChild(pixel)
-    }
-}
+let gridSize = 16
 
 container.addEventListener('mouseover', (event) => {
     if (event.target.classList == 'pixel') paintPixel(event.target)
 })
 
+button.addEventListener('click', () => {
+    gridSize = getGridSizeFormUser()
+    createGrid()
+})
+
+function getGridSizeFormUser() {
+    let newGridSize
+    do {
+        newGridSize = prompt('Choose a new grid size from 8 to 100')
+    } while (isNaN(newGridSize) || newGridSize < 8 || newGridSize > 100)
+    return newGridSize
+}
+
+function createGrid() {
+    container.replaceChildren()
+    const pixelSize = container.offsetWidth / gridSize
+    const pixelCount = gridSize ** 2
+    for (let i = 0; i < pixelCount; i++) {
+        const pixel = document.createElement('div')
+        pixel.style.width = pixelSize + 'px'
+        pixel.classList.add('pixel')
+        container.appendChild(pixel)
+    }
+}
+
 function paintPixel(pixel) {
-    pixel.style.backgroundColor = 'black'
+    pixel.style.backgroundColor = getRandomColor()
+}
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 createGrid()
